4e39bae56c7bf3e26241bf499b7e151b0bfbc826
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-10-09 15:54:34 +0400

    Added Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbars from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
     return (
         <>
             <Navbars/>
+            <Home/>
         </>
     );
 }
