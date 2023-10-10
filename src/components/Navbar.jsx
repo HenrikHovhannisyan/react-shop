d2ec732c1690a156467403b47791c7cd9d25c80a
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-10-10 15:25:41 +0400

    Fixed bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     return (
         <Navbar expand="lg" className="bg-body-light py-3 shadow-sm">
             <Container>
-                <Navbar.Brand href="/" className={'fw-bold fs-4'}>React-Shop</Navbar.Brand>
+                <NavLink to="/" className={'fw-bold fs-4 navbar-brand'}>React-Shop</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
@@ -19,16 +19,14 @@ const NavBar = () => {
                         <NavLink to="/about" className="nav-link">About</NavLink>
                         <NavLink to="/contact" className="nav-link">Contact</NavLink>
                     </Nav>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <div className="buttons">
-                        <Link to={"/login"} className={"btn btn-outline-dark"}>
+                    <div className="buttons d-flex">
+                        {/*<Link to={"/login"} className={"btn btn-outline-dark"}>
                             <i className="fa fa-sign-in me-1"/> Login
                         </Link>
                         <Link to={"/register"} className={"btn btn-outline-dark ms-2"}>
                             <i className="fa fa-user-plus me-1"/> Registration
-                        </Link>
-                        <Link to={"/cart"} className={"btn btn-outline-dark ms-2"}>
+                        </Link>*/}
+                        <Link to={"/cart"} className={"btn btn-outline-dark"}>
                             <i className="fa fa-shopping-cart me-1"/> Cart ({state.length})
                         </Link>
                     </div>
